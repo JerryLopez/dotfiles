c22cf80099c4b324411837cc35330404ee538a08
Jerry Lopez <jerrylopez@hey.com>
2021-07-05 09:24:37 -0500

    Adds homebrew bin to path

--- files/system/_path.zsh
+++ files/system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="$PATH:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin"
+export PATH="$PATH:./bin:/usr/local/bin:/usr/local/sbin:/opt/homebrew/bin:$ZSH/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
